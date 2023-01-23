9dcf2cc6f56dca212ca946fa6e914c18b77a33d5
OneRoomer <rene.elfinger@mailbox.org>
2023-01-23 20:01:37 +0100

    Add player choice and computer choice

---
+++ game.js
@@ -0,0 +1,8 @@
+//Player choice
+let playerChoice = prompt("Choose wisely Rock, Paper or Scissor!");
+
+//Computer choice
+const computerValue = ["Rock", "Paper", "Scissor"];
+const randomChoice = Math.floor(Math.random() * computerValue.length);
+//console.log(computerValue[randomChoice]);
+
