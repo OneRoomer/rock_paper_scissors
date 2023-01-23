6cbc2b0d503aee06f19b66e671af42c840d9d9d0
OneRoomer <rene.elfinger@mailbox.org>
2023-01-23 20:30:53 +0100

    Created play function

--- game.js
+++ game.js
@@ -1,8 +1,32 @@
 //Player choice
-let playerChoice = prompt("Choose wisely Rock, Paper or Scissor!");
+let playerProm = prompt("Choose wisely Rock, Paper or Scissor!");
+const playerChoice = playerProm.toString().toLowerCase();
 
 //Computer choice
-const computerValue = ["Rock", "Paper", "Scissor"];
+const computerValue = ["rock", "paper", "scissor"];
 const randomChoice = Math.floor(Math.random() * computerValue.length);
-//console.log(computerValue[randomChoice]);
+const computerChoice = computerValue[randomChoice].toString();
 
+console.log(playerChoice, computerChoice );
+
+
+//Play game
+function playRound (player, computer) {
+    if (player === "rock" && computer === "scissor") {
+        console.log("Player wins, Rock beats Scissor");
+    }
+    else if ( player === "paper" && computer === "rock") {
+        console.log("Player wins, Paper beats Rock");
+    }
+    else if (player === "scissor" && computer == "paper") {
+        console.log("Player wins, Scissor beats Paper");
+    }
+    else if (player === computer) {
+        console.log("Draw! Well played!");
+    }
+    else {
+        console.log("Computer wins! Try again!");
+    }
+}
+
+playRound (playerChoice, computerChoice);
