4459afb92b56be3a09bd5f6725a239daea010a89
OneRoomer <rene.elfinger@mailbox.org>
2023-01-26 08:32:09 +0100

    Added Playcount

--- game.js
+++ game.js
@@ -1,3 +1,26 @@
+
+
+//Play game
+function playRound (player, computer) {
+    if (player === "rock" && computer === "scissor") {
+        return("Player wins, Rock beats Scissor");
+    }
+    else if ( player === "paper" && computer === "rock") {
+        return("Player wins, Paper beats Rock");
+    }
+    else if (player === "scissor" && computer == "paper") {
+        return("Player wins, Scissor beats Paper");
+    }
+    else if (player === computer) {
+        return("Draw! Well played!");
+    }
+    else {
+        return("Computer wins! Try again!");
+    }
+}
+
+for (let i = 0; i < 5; i++){
+
 //Player choice
 let playerProm = prompt("Choose wisely Rock, Paper or Scissor!");
 const playerChoice = playerProm.toString().toLowerCase();
@@ -7,26 +30,5 @@ const computerValue = ["rock", "paper", "scissor"];
 const randomChoice = Math.floor(Math.random() * computerValue.length);
 const computerChoice = computerValue[randomChoice].toString();
 
-console.log(playerChoice, computerChoice );
-
-
-//Play game
-function playRound (player, computer) {
-    if (player === "rock" && computer === "scissor") {
-        console.log("Player wins, Rock beats Scissor");
-    }
-    else if ( player === "paper" && computer === "rock") {
-        console.log("Player wins, Paper beats Rock");
-    }
-    else if (player === "scissor" && computer == "paper") {
-        console.log("Player wins, Scissor beats Paper");
-    }
-    else if (player === computer) {
-        console.log("Draw! Well played!");
-    }
-    else {
-        console.log("Computer wins! Try again!");
-    }
+    console.log(playRound (playerChoice, computerChoice));
 }
-
-playRound (playerChoice, computerChoice);
