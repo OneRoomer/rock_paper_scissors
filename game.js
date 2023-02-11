294339effcf157741d651003b566f3868bd1d5d2
OneRoomer <rene.elfinger@mailbox.org>
2023-02-11 09:08:36 +0100

    Added div for displaying winner

--- game.js
+++ game.js
@@ -3,42 +3,40 @@
 const buttons = document.querySelectorAll('button');
 buttons.forEach((button) => {
     button.addEventListener('click', () => {
-        console.log(playRound(button.id, compChoice()));
+        playRound(button.id, compChoice());
     });
 });
 
-   /* //Player choice
-    let playerProm = prompt("Choose wisely Rock, Paper or Scissor!");
-    const playerChoice = playerProm.toString().toLowerCase();
-    */
+function displayResult (text) {
+    const resContainer = document.querySelector('#result');
+    resContainer.textContent = text;
+};
 
-    //Computer choice
-    function compChoice () {
+
+//Play game
+function playRound(player, computer) {
+    if (player === "rock" && computer === "scissor") {
+        displayResult("Player wins, Rock beats Scissor");
+    }
+    else if (player === "paper" && computer === "rock") {
+        displayResult("Player wins, Paper beats Rock");
+    }
+    else if (player === "scissor" && computer == "paper") {
+        displayResult("Player wins, Scissor beats Paper")
+    }
+    else if (player === computer) {
+        displayResult("Draw! Well played!");
+    }
+    else {
+        displayResult("Computer wins! Try again!");
+    }
+}
+
+//Computer choice
+function compChoice() {
     const computerValue = ["rock", "paper", "scissor"];
     const randomChoice = Math.floor(Math.random() * computerValue.length);
     const computerChoice = computerValue[randomChoice].toString();
     return computerChoice;
-    }
-    
-    //Function call
-    //console.log(playRound (playerChoice, computerChoice));
-
-
-//Play game
-function playRound (player, computer) {
-    if (player === "rock" && computer === "scissor") {
-        return("Player wins, Rock beats Scissor");
-    }
-    else if ( player === "paper" && computer === "rock") {
-        return("Player wins, Paper beats Rock");
-    }
-    else if (player === "scissor" && computer == "paper") {
-        return("Player wins, Scissor beats Paper");
-    }
-    else if (player === computer) {
-        return("Draw! Well played!");
-    }
-    else {
-        return("Computer wins! Try again!");
-    }
 }
+
